0e2a6e0f4d56e4de2843544856c116b322ae7d52
kovaaalex <kovalenko.alex04@gmail.com>
2025-06-10 17:15:53 +0300

    fix 3rd task

--- module1/[3]DataTypes.js
+++ module1/[3]DataTypes.js
@@ -1,4 +1,5 @@
 function sumFirstLastDigits(number){
+    if(number.toString().length === 1) return number;
     return number % 10 + +number.toString()[0];
 }
-console.log(sumFirstLastDigits(15));
+console.log(sumFirstLastDigits(1));
